cea03564d354522dfca6cbd178cb14f71ae6b8f3
RavinduTharanga <113649586+RavinduTharanga@users.noreply.github.com>
2023-06-20 13:24:42 -0400

    Update registration.cpp

--- registration.cpp
+++ registration.cpp
@@ -1,44 +1,23 @@
-#include <fstream>
 #include <iostream>
-#include <stdio.h>
-#include <string.h>
-
+#include <fstream>
+#include <cstring>
 using namespace std;
 
-int main(){
-
-    char data[15];
-    int n = 0, option =0 ,count_n =0;
-    string empty = "00";
-    string line;
-    string proctor = "";
-    ifstream f("Example.txt");
-
-    for (int i =0; std::getline(f,line);i++){
-
-        count_n++;
-    }
-    while (option!=6)
-    {
-        cout << "\nAvailable operations: \n1. Add New "
-                "Students\n2."
-             << "Student Login\n3. Faculty Login\n4. "
-                "Proctor Login\n5. Admin View\n"
-             << "6. Exit\nEnter option: ";
+void menu(){
+    int option;
+        cout<<"\n\n---------Student Database System --------\n\n";
+        cout<<"1 : Add New Student\n";
+        cout<<"2 : Student Login\n";
+        cout<<"3 : Faculty Loging\n";
+        cout<<"4 : Admin View\n";
+        cout<<"5 : Exit\n";
+        cout<< "Please Enter Number :";  
         cin >> option;
-
-        if(option ==1){
-
-            cout << "Enter the number of students: ";
-            cin >> n;
-
-            count_n = count_n + n;
-        }
-    }
-    
+}
 
 
-
+int main(){
+   menu();
 
 
 }
